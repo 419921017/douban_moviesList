0d0d80840b3a376e8b815aa255c5043043500fe4
419921017 <419921017@users.noreply.github.com>
2016-09-02 11:35:42 +0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Seele on 2016/9/3.
+ */
+
+
+angular.module( 'appModule', [] )
+.controller( 'app.controller', [ '$scope', function($scope){
+
+} ] )
---
+++ js/area/moveList/moveListController.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Seele on 2016/9/3.
+ */
